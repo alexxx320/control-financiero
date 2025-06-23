28c9f15c2ce777a778018b24fa783ea3faada2d7
alexxx320 <williamtermal2021@itp.edu.co>
2025-06-23 04:13:08 -0500

    arreglando produccion frontend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://hcontrol-financiero-production-2ab8.up.railway.app/api'
+  apiUrl: 'https://control-financiero-production-2ab8.up.railway.app/api'
 };
